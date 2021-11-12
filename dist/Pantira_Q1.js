d09228fa34bce71959dc931d656d56d5cef6bbb5
PantiraR818 <pantiraploy818@gmail.com>
2021-11-12 17:53:55 +0700

    Q1

---
+++ dist/Pantira_Q1.js
@@ -0,0 +1,5 @@
+//Q1 สร้าง Tuple ของนศ [id,name,mid,final,project,grade]
+//                      1,"pantira",20,30,35,""
+//แสดง Output :<id>:<name>, Total: <mid+final+project>,Grade: <grade>
+//Grad 4,3.5,3,2.5,2,1.5,1,0
+//FileName : Pantira_Q1.ts
